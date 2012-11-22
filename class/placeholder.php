99c5c4f7957ff179a653bed4aa2044365bc7f3d4
Martin Blackburn <walking_fishy@hotmail.com>
2012-11-22 13:31:32 +0000

    getting random image from directory and displaying it

--- class/placeholder.php
+++ class/placeholder.php
@@ -2,13 +2,64 @@
 
 class Placeholder 
 {
+    private $images = array();
+    private $imageDir;
+    
+    /**
+	 * Construct the PlaceHoler calls
+	 */
+    function __construct()
+    {
+        //set default image dir
+        $this->imageDir = getcwd() . "/images/";
+        
+        //add allimages to the array
+        $this->getImages();
+    }
+    
+    
+    /**
+    * Add all images to the images array
+    */
+    private function getImages()
+    {           
+        $dirhandler = opendir($this->imageDir);
+        
+        while ($file = readdir($dirhandler))
+        {
+            clearstatcache();
+            
+            if(is_file($this->imageDir . '/' . $file))
+            {    
+                if ($file != '.' && $file != '..')
+                {
+                    $this->images[] = $file;
+                }
+            }
+        }
+        
+        closedir($dirhandler);
+    }
+    
+    
+    /**
+    * Get a random image from images array
+    * 
+    * @return string
+    */
+    private function getImage()
+    {
+        return $this->images[array_rand($this->images)];
+    }
+    
+    
     /**
     * Display image
     */
-    function render() 
+    public function render() 
     {
         header('Content-type: image/png');
         
-        readfile(dirname(__FILE__) . "/../images/yetti-upgrade.png");
+        readfile($this->imageDir . $this->getImage());
     }
 }
