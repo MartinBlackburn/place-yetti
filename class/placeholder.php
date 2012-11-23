9b357f65815ea4280356f0ece058095779a7e079
Martin Blackburn <walking_fishy@hotmail.com>
2012-11-23 15:07:17 +0000

    added image resizing functions

--- class/placeholder.php
+++ class/placeholder.php
@@ -3,9 +3,12 @@
 class Placeholder 
 {
     private $images = array();
-    private $imageDir;
+    private $imagesDir;
+    private $image;
     private $imageWidth = 300;
     private $imageHeight = 300;
+    private $imagePath;
+    private $imageType;
     
     /**
 	 * Construct the PlaceHoler calls
@@ -13,7 +16,7 @@ class Placeholder
     function __construct()
     {        
         //set default image dir
-        $this->imageDir = getcwd() . "/images/";
+        $this->imagesDir = getcwd() . "/images/";
         
         //add all images to the array
         $this->setImages();
@@ -28,13 +31,13 @@ class Placeholder
     */
     private function setImages()
     {           
-        $dirhandler = opendir($this->imageDir);
+        $dirhandler = opendir($this->imagesDir);
         
         while ($file = readdir($dirhandler))
         {
             clearstatcache();
             
-            if(is_file($this->imageDir . '/' . $file))
+            if(is_file($this->imagesDir . '/' . $file))
             {    
                 if ($file != '.' && $file != '..')
                 {
@@ -54,8 +57,8 @@ class Placeholder
     {   
         //get request url
         $requestURI = explode("/", $_SERVER["REQUEST_URI"]);
-        $width = 300;
-        $height = 300;
+        $width = (int)$requestURI[1];
+        $height = (int)$requestURI[2];
         
         //set width if numeric
         if(is_numeric($width))
@@ -76,12 +79,79 @@ class Placeholder
     
     /**
     * Get a random image from images array
-    * 
-    * @return string
     */
-    private function getImage()
+    private function getRandomImage()
+    {   
+        $imagePath =  $this->imagesDir . $this->images[array_rand($this->images)];
+        
+        $this->imagePath = $imagePath;
+        $this->imageType = image_type_to_mime_type(exif_imagetype($imagePath));
+        $this->image = ImageCreateFromString(file_get_contents($imagePath));
+    }
+    
+    
+    /**
+    * Scales the image based on a new width, maintaining the original aspect ratio.
+    * 
+    * @param int $width
+    */
+    private function resizeByWidth($width) 
     {
-        return $this->images[array_rand($this->images)];
+        $ratio = $this->imageWidth / $width;
+        
+        $this->imageHeight = round($this->imageHeight * $ratio);
+    }
+    
+    
+    /**
+     * Scales the image based on a new height, maintaining the original aspect ratio.
+     * 
+     * @param int $height
+     */
+    private function resizeByHeight($height) 
+    {
+        $ratio = $this->imageHeight / $height;
+        
+        $this->imageWidth = $this->imageWidth * $ratio;
+    }
+    
+    
+    /**
+    * Resize image to values given
+    */
+    private function resizeImage()
+    {
+        //get source with and height
+        list($sourceWidth, $sourceHeight) = getimagesize($this->imagePath);
+        
+        //get ratios
+        $widthRatio = $this->imageWidth / $sourceWidth;
+        $heightRatio = $this->imageHeight / $sourceHeight;
+        
+        //check ratios and resize by the bigger one
+        if($widthRatio > $heightRatio) {
+            $this->resizeByWidth($sourceWidth);
+        } else {
+            $this->resizeByHeight($sourceHeight);
+        }
+        
+        //create blank image with the correct size
+        $imageResized = imagecreatetruecolor($this->imageWidth, $this->imageHeight);
+        
+        //copy new image at the new size from the source image to the new blank image
+        imagecopyresampled($imageResized, $this->image, 0, 0, 0, 0, $this->imageWidth, $this->imageHeight, $sourceWidth, $sourceHeight);
+        
+        //update the image
+        $this->image = $imageResized;
+    }
+    
+    
+    /**
+    * Set the header based on image type
+    */
+    private function setHeader()
+    {
+        header('Content-type: ' . $this->imageType);
     }
     
     
@@ -89,9 +159,14 @@ class Placeholder
     * Display image
     */
     public function render() 
-    {
-        header('Content-type: image/png');
+    {        
+        $this->getRandomImage();
         
-        readfile($this->imageDir . $this->getImage());
+        $this->resizeImage();
+        
+        $this->setHeader();
+        
+        imagepng($this->image);
+        imagedestroy($this->image);        
     }
 }
